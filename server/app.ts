2cda7590041330afd9507c72d41bb69c27c198af
Alexander Everaert <alexander.everaert@student.howest.be>
2022-01-12 16:26:28 +0100

    added auth

--- server/app.ts
+++ server/app.ts
@@ -1,11 +1,24 @@
 import express, { Request, Response } from 'express'
+import dotenv from 'dotenv'
+
+import credential from './auth/application-credentials.json'
+import admin, { auth } from 'firebase-admin'
+import { initializeApp, ServiceAccount } from 'firebase-admin/app'
+import authMiddleware from './auth/firebaseAuthMiddleware'
 
 // APP SETUP
+dotenv.config()
+initializeApp({
+  credential: admin.credential.cert(credential as ServiceAccount)
+})
+
+
 const app = express(),
-  port = process.env.PORT || 3000
+  port = process.env.PORT || 3001
 
 // MIDDLEWARE
 app.use(express.json()) // for parsing application/json
+app.use(authMiddleware)
 
 // ROUTES
 app.get('/', (request: Request, response: Response) => {
