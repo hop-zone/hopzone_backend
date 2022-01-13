752a974d3222f3b34b7b817ac1ac46eee08be2b5
Alexander Everaert <alexander.everaert@student.howest.be>
2022-01-13 17:55:17 +0100

    socket auth middleware

--- server/app.ts
+++ server/app.ts
@@ -4,7 +4,10 @@ import dotenv from 'dotenv'
 import credential from './auth/application-credentials.json'
 import admin, { auth } from 'firebase-admin'
 import { initializeApp, ServiceAccount } from 'firebase-admin/app'
+import { createServer } from "http"
 import authMiddleware from './auth/firebaseAuthMiddleware'
+import { Server, Socket } from 'socket.io'
+import { SocketController } from './controller/socketController'
 
 // APP SETUP
 dotenv.config()
@@ -13,19 +16,45 @@ initializeApp({
 })
 
 
-const app = express(),
-  port = process.env.PORT || 3001
+const port = process.env.PORT || 3001
+const app = express()
+const httpServer = createServer(app)
+const io = new Server(httpServer, { cors: { origin: '*', methods: ['GET', 'POST'] } })
 
-// MIDDLEWARE
-app.use(express.json()) // for parsing application/json
-app.use(authMiddleware)
 
-// ROUTES
-app.get('/', (request: Request, response: Response) => {
-  response.send(`Welcome, just know: you matter!`)
+// io.use((socket, next) => {
+//   console.log(socket.request.headers.authorization);
+  
+// })
+io.use(authMiddleware)
+
+io.on("connection", (socket) => {
+  const controller = new SocketController(io, socket)
+
+  console.log("new client");
+  
+  socket.emit('b2f_connection', 'Successfully connected')
+  socket.on('f2b_newLobby', controller.createLobby)
+
 })
 
-// APP START
-app.listen(port, () => {
-  console.info(`\nServer 👾 \nListening on http://localhost:${port}/`)
+httpServer.listen(port, () => {
+  console.info('The socket IO server is listening!')
 })
+
+// const app = express(),
+//   port = process.env.PORT || 3001
+
+// // MIDDLEWARE
+// app.use(express.json()) // for parsing application/json
+// app.use(authMiddleware)
+
+// // ROUTES
+// app.get('/', (request: Request, response: Response) => {
+//   response.send(`Welcome, just know: you matter!`)
+// })
+
+// // APP START
+// app.listen(port, () => {
+//   console.info(`\nServer 👾 \nListening on http://localhost:${port}/`)
+// })
